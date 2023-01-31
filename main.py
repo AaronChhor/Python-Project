8338930f491c1681fdb4ab89334ee1721f43c161
Aaron Chhor <106990534+AaronChhor@users.noreply.github.com>
2023-01-31 06:49:29 +0000

    url data

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import requests
 import json
 import pandas as pd
 
-url data = '@opensky-network.org/api/states/all?'
-response = requests.get(url_data).json()
+longmin, latmin=1,2
+longmax, latmax=1,2
+username=""
+password=""
+
+urldata = 'https://'+username+':'+password+'@opensky-network.org/api/states/all?'+'lamin='+str(latmin)+'&lomin='+str(longmin)+'&lamax='+str(latmax)+'&lomax='+str(longmax)
+response = requests.get(urldata).json()
 
 name = ['icao24','callsign','origin_country','time_position','last_contact','long','lat','baro_altitude','on_ground','velocity',     'true_track','vertical_rate','sensors','geo_altitude','squawk','spi','position_source']
 flight_df=pd.DataFrame(response['states'])
