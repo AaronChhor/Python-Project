152d73cabd1a9507f61230fd59867bd92ba2c149
Aaron Chhor <106990534+AaronChhor@users.noreply.github.com>
2023-02-07 04:56:34 +0000

    Goal switch

--- main.py
+++ main.py
@@ -18,8 +18,38 @@ flight_df.columns=name
 flight_df=flight_df.fillna('No Data')
 flight_df.head()
 
-origin_country = 'origin_country'
 
-country = origin_country
-for i in (response):
-  country = i
+print(response)
+
+def flight_tracking(doc):
+    
+    flight_source = ColumnDataSource({
+        'icao24':[],'callsign':[],'origin_country':[],
+        'time_position':[],'last_contact':[],'long':[],'lat':[],
+        'baro_altitude':[],'on_ground':[],'velocity':[],'true_track':[],
+        'vertical_rate':[],'sensors':[],'geo_altitude':[],'squawk':[],'spi':[],'position_source':[],'x':[],'y':[],
+        'rot_angle':[],'url':[]
+
+    }
+            
+#origin_country = 'origin_country'
+ #Country = input("Countryname: ")
+  
+#US_planes = int(0)
+#for i in response:
+#  if i == "United States":
+#    US_planes+=1
+  
+#print(US_planes)
+
+#for i in flight_source:
+ # print(time_position)
+
+
+#print(response)
+
+
+#country = origin_country
+#for i in (response):
+#  country = i
+
