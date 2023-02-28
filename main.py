52439a5b8a7c82910c9551b0c649cc97692ff3d1
Aaron Chhor <106990534+AaronChhor@users.noreply.github.com>
2023-02-28 06:05:23 +0000

    Reset

--- main.py
+++ main.py
@@ -1,23 +1,63 @@
-import requests
-import json
 import pandas as pd
-import http.client
-#pd.read_json('@opensky-network.org/api/states/all')
 
+# Read the files and import all rows.
+df_18 = pd.read_csv('delays_2018.csv')
+#df_19 = pd.read_csv('delays_2019.csv')
 
-conn = http.client.HTTPSConnection("airport-info.p.rapidapi.com")
+# Concatenate the 2018 and 2019 data into a single DataFrame.
+df = pd.concat([df_18,], ignore_index=True)
 
-headers = {
-    'X-RapidAPI-Key': "5a89e79fe4msh7410d61b0054b56p1bff01jsn662d1919dcf9",
-    'X-RapidAPI-Host': "airport-info.p.rapidapi.com"
-    }
+# Print out the number of rows imported from the files.
+print('Number of Rows: ' + str(len(df_18)))
 
-conn.request("GET", "/airport?iata=JFK&icao=KJFK", headers=headers)
+# Change the data type of the 'month' column to date and change the format to YYYY-M (e.g. 2018-1).
+df['date'] = pd.to_datetime(df['date'], format='%Y-%m').dt.strftime('%Y-%m')
 
-res = conn.getresponse()
-data = res.read()
+# Remove rows containing invalid data.
+df = df[(df['date'] >= '2018-01') & (df['date'] <= '2019-12') & (df['arr_flights'].notnull())
+              & (df['carrier'].notnull()) & (df['carrier_name'].notnull()) 
+              & (df['airport'].notnull()) & (df['airport_name'].notnull())]
 
-print(data.decode("utf-8"))
+# Print out the number of rows remaining in the dataset.
+print('Number of Rows: ' + str(len(df)))
+
+# Identify the airports in the state of Tennessee.
+df['TN'] = df['airport_name'].apply(lambda x: x.find('TN'))
+
+# Create a set of airport names (to eliminate the duplicates).
+airports = set(df[df['TN'] != -1]['airport_name'])
+
+# Display the list of airports.
+print('Tennessee Airports:')
+print(airports)
+
+# Read the coordinates file and import all rows.
+df_coords = pd.read_csv('airport_coordinates.csv')
+
+# Create a new DataFrame with airport codes and names.
+df_airports = df[['airport', 'airport_name']].drop_duplicates().reset_index(drop=True)
+
+# Merge the coordinates DataFrame with the airports DataFrame.
+df_airports = pd.merge(df_airports, df_coords, on='airport')
+
+# This code was required due to an Anaconda issue.  You may not need it depending on your environment.
+import os
+os.environ["PROJ_LIB"] = "C:\\Users\\Nick\\anaconda3\\envs\\sandbox\\Library\\share\\basemap";
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+
+# Ready the Basemap for display.
+fig = plt.figure(figsize=(16, 16))
+m = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
+        projection='lcc',lat_1=32,lat_2=45,lon_0=-95)
+
+# Load the shapefile to display the outlines of the US states.
+m.readshapefile('st99_d00', name='states', drawbounds=True)
+
+# Plot the airports on the map.
+m.scatter(df_airports['long'].values, df_airports['lat'].values, latlon=True)
+plt.show()
 
 #response = requests.get("airport-info.p.rapidapi.com")
 #planes = response.json()
@@ -29,7 +69,6 @@ print(data.decode("utf-8"))
 #urldata = 'https://'+username+':'+password+'@opensky-network.org/api/states/all?'+'lamin='+str(latmin)+'&lomin='+str(longmin)+'&lamax='+str(latmax)+'&lomax='+str(longmax)
 #response = requests.get(urldata).json()
 
-name = ['icao24','callsign','origin_country','time_position','last_contact','long','lat','baro_altitude','on_ground','velocity',     'true_track','vertical_rate','sensors','geo_altitude','squawk','spi','position_source']
 
 #flight_df=pd.DataFrame(response['states'])
 #flight_df=flight_df.loc[:,0:16]
