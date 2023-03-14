e864ec44b263c69aa3fee635e9c600a248a8545d
Aaron Chhor <106990534+AaronChhor@users.noreply.github.com>
2023-03-14 16:11:20 +0000

    Data set

--- main.py
+++ main.py
@@ -1,43 +1,63 @@
 import pandas as pd
 
-flights_2018 = pd.read_csv('delay_2018.csv')
-flights_2019= pd.read_csv('delay_2019.csv')
+flights_delay = pd.read_csv('airline_delay_causes.csv')
+#flights_2018 = pd.read_csv('delay_2018.csv')
+#flights_2019= pd.read_csv('delay_2019.csv')
 
+df = pd.concat([flights_delay], ignore_index=True)
+#df = pd.concat([flights_2018, flights_2019], ignore_index=True)
 
-df = pd.concat([flights_2018, flights_2019], ignore_index=True)
+print('Number of Rows: ' + str(len(flights_delay)))
 
-print('Number of Rows: ' + str(len(flights_2018)+len(flights_2019)))
+df['year'] = pd.to_datetime(df['year'], format='%Y-%m').dt.strftime('%Y-%m')
 
-df['date'] = pd.to_datetime(df['date'], format='%Y-%m').dt.strftime('%Y-%m')
-
-df = df[(df['date'] >= '2018-01') & (df['date'] <= '2019-12') & (df['arr_flights'].notnull())
+df = df[(df['year'] >= '2016') & (df['arr_flights'].notnull())
               & (df['carrier'].notnull()) & (df['carrier_name'].notnull()) 
               & (df['airport'].notnull()) & (df['airport_name'].notnull())]
 
-print('Number of Rows: ' + str(len(df)))
-
-df['TN'] = df['airport_name'].apply(lambda x: x.find('TN'))
-df['NY'] = df['airport_name'].apply(lambda x: x.find('NY'))
-
-airports_TN = set(df[df['TN'] != -1]['airport_name'])
-airports_NY = set(df[df['NY'] != -1]['airport_name'])
-
-print('Tennessee Airports:')
-print(airports_TN)
-print(airports_NY)
-
-flight_coords = pd.read_csv('coords.csv')
-
-flight_airports = df[['airport', 'airport_name']].drop_duplicates().reset_index(drop=True)
-
-df_airports = pd.merge(flight_airports, flight_coords, on='airport')
-
-pd.crosstab(df['carrier'], df['airport'], values=df['arr_diverted'], aggfunc='sum').fillna('')
-flights_f = df[(df['date'] >= '2019-01') & (df['date'] <= '2019-12') & (df['airport'] == 'JFK') 
-          & (df['carrier_ct'] > 0) & (df['weather_ct'] > 0)]
+#print('Number of Rows: ' + str(len(df)))
 
 
-print("Number of Delays: " + str(flights_f['carrier_ct'].sum()  + flights_f['weather_ct'].sum()))
+spirit_airlines = 0
+for x in 'airport_name':
+    if 'airport_name' == 'Spirit Air Lines':
+      spirit_airlines = spirit_airlines +1
+print("Number of Spirit Airlines airports: " + spirit_airlines)
+
+
+#print("Number of Delays: " + str(flights_f['carrier_ct'].sum()  + flights_f['weather_ct'].sum()))
+
+
+
+
+
+
+
+
+
+
+#df['TN'] = df['airport_name'].apply(lambda x: x.find('TN'))
+#df['NY'] = df['airport_name'].apply(lambda x: x.find('NY'))
+
+#airports_TN = set(df[df['TN'] != -1]['airport_name'])
+#airports_NY = set(df[df['NY'] != -1]['airport_name'])
+
+#print('Tennessee Airports:')
+#print(airports_TN)
+#print(airports_NY)
+
+#flight_coords = pd.read_csv('coords.csv')
+
+#flight_airports = df[['airport', 'airport_name']].drop_duplicates().reset_index(drop=True)
+
+#df_airports = pd.merge(flight_airports, flight_coords, on='airport')
+
+#pd.crosstab(df['carrier'], df['airport'], values=df['arr_diverted'], aggfunc='sum').fillna('')
+#flights_f = df[(df['date'] >= '2019-01') & (df['date'] <= '2019-12') & (df['airport'] == 'JFK') 
+        #  & (df['carrier_ct'] > 0) & (df['weather_ct'] > 0)]
+
+
+
 
 
 #response = requests.get("airport-info.p.rapidapi.com")
