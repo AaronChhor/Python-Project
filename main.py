991fc572a7444e6b5eb0d2ea115fbf92eda0f2ac
Aaron Chhor <>
2023-04-07 06:09:39 +0000

    Spirit Air Lines

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import pandas as pd
 df = pd.read_csv('airline_delay_causes.csv')
 #df.head(3)
 #df["arr_delay"].head()
-Spirit_df= df[ df['region']=="Spirit Air Lines"]
-Spirit_df.head()
-Spirit_df.index
-Spirit_df.set_index("Year", inplace=True)
-Spirit_df.plot()
+#Spirit_df= df[ df['airport_name']=='Spirit Air Lines']
+#Spirit_df.head()
+#Spirit_df.index
+#Spirit_df.set_index('arr_delay', inplace=True)
+#Spirit_df.plot()
+
+print (df.shape)
+print (df.columns)
+print (df.head(n=5))
+print (df[["airport_name", "carrier"]].head(n=10))
+print ("xxx")
 
 
 print('Number of Rows: ' + str(len(df)))
@@ -22,15 +28,32 @@ df = df[(df['year'] >= '2016') & (df['arr_flights'].notnull())
 print(df.values)
 
 
-#for i in df.keys():
-key = "Spirit Air Lines"
-Spirit_airlines = 0
-for i in df.values:
-  if i == key:
-    Spirit_airlines+=1
-print(Spirit_airlines)
+Avg_Spirit_count = 0
 
-Spirit_avg = Spirit_airlines/Rows
+with open('airline_delay_causes.csv', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        if "Spirit Air Lines" in line:
+            Avg_Spirit_count += 1
+
+print("Number of occurrences of 'Spirit Air Lines':", Avg_Spirit_count)
+
+if Avg_Spirit_count>0:
+  Avg_Spirit_count/=Rows
+  print("Percentage of flights made by 'Spirit Air Lines':", Avg_Spirit_count*100, "%")
+
+
+
+
+#for i in df.keys():
+#key = "Spirit Air Lines"
+#Spirit_airlines = 0
+#for i in df.values:
+#  if i == key:
+#    Spirit_airlines+=1
+#print(Spirit_airlines)
+
+#Spirit_avg = Spirit_airlines/Rows
 
 #Find Average minutes of delay
 key = 'Spirit Air Lines'
@@ -41,9 +64,7 @@ for i in df.values:
     Avg_Spirit_del+= 'arr_delay'
     Avg_Spirit_count+=1
     
-if Avg_Spirit_count>0:
-  Avg_Spirit_del/=Avg_Spirit_count
-  print(Avg_Spirit_del)
+
 
 
 
